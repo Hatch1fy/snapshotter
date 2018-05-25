c071b3b33bfbdc7ae6acb07ffb1032df8cc5fc54
itsmontoya <josh@meteora.co>
2018-05-25 16:56:58 -0700

    Implement config

---
+++ config.go
@@ -0,0 +1,38 @@
+package snapshotter
+
+import (
+	"time"
+
+	"github.com/Hatch1fy/errors"
+)
+
+// NewConfig will return a new default Config with the given name and extension
+func NewConfig(name, ext string) (cfg Config) {
+	cfg.Name = name
+	cfg.Extension = ext
+	cfg.Interval = Minute
+	cfg.Truncate = Hour
+	return
+}
+
+// Config are the basic configuration settings for snapshotter
+type Config struct {
+	Name      string
+	Extension string
+	Interval  time.Duration
+	Truncate  time.Duration
+}
+
+// Validate will validate a Config
+func (c *Config) Validate() (err error) {
+	var errs errors.ErrorList
+	if !isValidTruncate(c.Truncate) {
+		errs = append(errs, ErrInvalidTruncate)
+	}
+
+	if c.Interval < Second {
+		errs = append(errs, ErrInvalidInterval)
+	}
+
+	return errs.Err()
+}
