6468804c34575239fcf862a73c07c68e2806b2cf
Josh Montoya <josh@montoya.io>
2018-10-12 08:56:45 -0700

    Add testing to next method

--- backends/s3_test.go
+++ backends/s3_test.go
@@ -20,7 +20,6 @@ func TestS3(t *testing.T) {
 	creds := credentials.NewEnvCredentials()
 	region := os.Getenv("AWS_REGION")
 	bucket := os.Getenv("AWS_BUCKET")
-
 	cfg := aws.Config{
 		Region:      aws.String(region),
 		Credentials: creds,
@@ -31,15 +30,23 @@ func TestS3(t *testing.T) {
 	}
 
 	// Attempt to write to test key
-	if err = s3.WriteTo("test", func(w io.Writer) (err error) {
-		_, err = w.Write([]byte("hello world"))
+	if err = s3.WriteTo("test_log_1.log", func(w io.Writer) (err error) {
+		_, err = w.Write([]byte("hello world\n"))
+		return
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Attempt to write to test key
+	if err = s3.WriteTo("test_log_2.log", func(w io.Writer) (err error) {
+		_, err = w.Write([]byte("hello world\n"))
 		return
 	}); err != nil {
 		t.Fatal(err)
 	}
 
 	// Attempt to read from test key
-	if err = s3.ReadFrom("test", func(r io.Reader) (err error) {
+	if err = s3.ReadFrom("test_log_1.log", func(r io.Reader) (err error) {
 		// Create buffer to writer to
 		buf := bytes.NewBuffer(nil)
 		// Copy reader bytes to buffer
@@ -48,12 +55,29 @@ func TestS3(t *testing.T) {
 		}
 
 		// Ensure buffer value is our expected value
-		if buf.String() != "hello world" {
+		if buf.String() != "hello world\n" {
 			return fmt.Errorf("invalid value, expected \"%s\" and received \"%s\"", "hello world", buf.String())
 		}
 
 		return
 	}); err != nil {
 		t.Fatal(err)
+	}
+
+	var nextKey string
+	if nextKey, err = s3.Next("test", ""); err != nil {
+		t.Fatal(err)
+	} else if nextKey != "test_log_1.log" {
+		t.Fatalf("invalid key value, expected \"%s\" and received \"%s\"", "test_log_1.log", nextKey)
+	}
+
+	if nextKey, err = s3.Next("test", nextKey); err != nil {
+		t.Fatal(err)
+	} else if nextKey != "test_log_2.log" {
+		t.Fatalf("invalid key value, expected \"%s\" and received \"%s\"", "test_log_2.log", nextKey)
+	}
+
+	if nextKey, err = s3.Next("test", nextKey); err != io.EOF {
+		t.Fatalf("io.EOF expected, received: %v", err)
 	}
 }
